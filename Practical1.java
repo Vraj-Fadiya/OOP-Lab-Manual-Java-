583cf38de80f0bf59744d5dbd015be2fa30da9e0
Fadiya Vraj <vrajfadiya07@gmail.com>
2026-04-26 11:25:23 +0530

    Add meter to feet converter - Lab Q1
    Java program that takes distance in meters as user input 
    and converts it to feet (1 meter = 3.28084 feet), 
    displaying the result formatted to two decimal places.

---
+++ Practical1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Practical1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enrollment Number: 240390107020");
+        System.out.println("Enter The Meters To Convert: ");
+        double meter = sc.nextDouble();
+
+        double feet = meter * 3.28084;
+
+        System.out.printf("Converted Feet: %.3f ", feet);
+        sc.close();
+        System.out.println("\nVraj Fadiya | 240390107007");
+    }
+}
