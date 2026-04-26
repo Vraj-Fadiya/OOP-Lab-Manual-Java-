02a7a7941f58d341a2ddd165164dc02e982b70e8
Fadiya Vraj <vrajfadiya07@gmail.com>
2026-04-26 11:35:10 +0530

    Command-line args calculator - Lab Q8
    Java program that accepts two numbers as command-line arguments,
    converts them to numeric types, performs sum and product
    calculations, and prints the results to the console.

---
+++ CommandLineCalculation.java
@@ -0,0 +1,12 @@
+class CommandLineCalculation {
+    public static void main(String[] args) {
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Vraj Fadiya | 240390107007");
+    }
+}
